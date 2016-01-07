2d1fcf94607b0bb225a2dab246390d37e27a55a4
RyanBeatty <rvbeatty@email.wm.edu>
2016-01-07 00:46:24 -0500

    small refactoring stuff

--- src/AI/MCTS.hs
+++ src/AI/MCTS.hs
@@ -32,7 +32,7 @@ gameNode value count action curState = case curState of
 newGameNode :: Action -> GameState -> SearchNode
 newGameNode action curState = gameNode 0 0 action curState
 
-mctsSearch :: StdGen -> TreePos Full SearchNode -> (StdGen, TreePos Full SearchNode)
+mctsSearch :: TreePos Full SearchNode -> StdGen -> (TreePos Full SearchNode, StdGen)
 mctsSearch = (,) . backUp . defaultPolicy . treePolicy
 
 ------------------methods implementing treePolicy------------------
@@ -40,13 +40,13 @@ mctsSearch = (,) . backUp . defaultPolicy . treePolicy
 -- | Search faze of a single iteration of MCTS. Decends down the tree
 -- | deciding until it finds either a terminal node or a node that is not
 -- | fully expanded.
-treePolicy :: StdGen -> TreePos Full SearchNode -> (TreePos Full SearchNode, StdGen)
-treePolicy gen searchTree
+treePolicy :: TreePos Full SearchNode -> StdGen -> (TreePos Full SearchNode, StdGen)
+treePolicy searchTree gen
   -- If a node is a TerminalNode, then stop search
   | isTerminal tree'      = (searchTree, gen)
 
   -- If a Node is FullyExpanded, then continue search with its best child
-  | isFullyExpanded tree' = treePolicy gen bChild
+  | isFullyExpanded tree' = treePolicy bChild gen
 
   -- otherwise, expand the current node and stop search
   | otherwise             = expand searchTree gen
